303ea4f11f2f68a3a74918491c4b6324c9609060
Alfarabi11 <94303703+Alfarabi11@users.noreply.github.com>
2021-11-18 21:10:08 +0700

    Update and rename README.md to biodata.py

---
+++ biodata.py
@@ -0,0 +1,12 @@
+Nama = 'Mohammad Alfarab Susanto'
+NIM = 212410102090
+Asal = 'Situbondo'
+Program_Studi = 'Teknologi Informasi'
+Fakultas = 'Ilmu Kompiter'
+
+print(
+  'Halo!! Perkenalkan namaku', Nama,', NIM', NIM,',' 
+  'dari program studi', Program_Studi,', Fakultas', Fakultas,
+  'Universitas Jember.' 'Aku berasal dari', Asal,'.'
+  'Salam Kenal Gaes!!!'
+)
